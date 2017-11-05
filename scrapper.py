9a950c4f5e39249b1ef5c01aabbe8ffd6a0f6d3d
keyuls <keyul@asu.edu>
2017-11-05 13:37:42 -0700

    embedly scrappeer added

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import urllib2
 
 data = []
 quote_page = ['https://www.compareremit.com/compare-money-transfer-services-from-usd-to-india/?amt=1000&sc=USD&rc=INR']
@@ -8,7 +7,7 @@ quote_page = ['https://www.compareremit.com/compare-money-transfer-services-from
 def scrapSites():
     for pg in quote_page:
         #page = urllib2.urlopen(pg)
-        driver = webdriver.Firefox(executable_path=r'C:\Users\pax\Downloads\geckodriver-v0.19.0-win64\geckodriver.exe')
+        driver = webdriver.Firefox(executable_path=r'C:\Users\keyul\Downloads\geckodriver-v0.19.1-win64\geckodriver.exe')
         driver.get(pg)
         page = driver.page_source
 
@@ -22,8 +21,24 @@ def scrapSites():
             name_box = cpy.find_all('span', attrs={'class': 'rate', 'title': 'Guaranteed rate'})
             data = [span.get_text() for span in name_box]
             for d in data:
-                print d
+                print (d)
             print ("----")
 
 
-scrapSites()
+#scrapSites()
+quote_page = ['https://www.producthunt.com/upcoming/darian']
+def scrapEmbedly():
+    for pg in quote_page:
+        #page = urllib2.urlopen(pg)
+        url = 'http://embed.ly/code?url='+pg
+        driver = webdriver.Firefox(executable_path=r'C:\Users\keyul\Downloads\geckodriver-v0.19.1-win64\geckodriver.exe')
+        driver.get(url)
+        page = driver.page_source
+
+        soup = BeautifulSoup(page, 'html.parser')
+        company = soup.find('div', attrs ={'id': 'snippetDiv'})
+        print(company.get_text())
+        print ("----")
+
+
+scrapEmbedly()
